6229c7e03d611a1484c86503ff4c4f4d5542998a
kimsunhong <>
2021-02-01 20:43:39 +0900

    add index page

---
+++ BaseBallGame/indexPage.swift
@@ -0,0 +1,23 @@
+//
+//  indexPage.swift
+//  BaseBallGame
+//
+//  Created by kimsunhong on 2021/02/01.
+//  Copyright © 2021 hongnemo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class IndexPage : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func baseBallButton(_ sender: UIButton) {
+        print("HI")
+        
+    }
+    
+}
